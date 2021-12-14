32c45c4eb30081cc90acdfcab5a4e7f4d48c7129
Yuri Correa <yurifcorrea@gmail.com>
2021-12-14 14:58:07 -0300

    commit 16: listing repos w/ .map()

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,10 @@
 export function RepositoryItem(props) {
   return (
     <li>
-      <strong>{props.repository?.name ?? 'default'}</strong>
-      <p>{props.repository?.description ?? 'default'}</p>
+      <strong>{props.repository?.name ?? '(no name)'}</strong>
+      <p>{props.repository?.description ?? '(no description)'}</p>
 
-      <a href={props.repository?.link ?? 'default'}>
+      <a href={props.repository?.html_url ?? '#'}>
         Acessar repositório
       </a>
     </li>
